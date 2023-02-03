23dd25643f532303df71b007d89f308166abc83e
const604 <116814907+const604@users.noreply.github.com>
2023-02-03 18:36:35 +0200

    Завдання 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,17 @@
-const counterValue = 
+const refs = {
+  counter: document.querySelector("#counter"),
+  counterValueRef: document.querySelector("#value"),
+  decrementBtn: document.querySelector('button[data-action="decrement"]'),
+  incrementBtn: document.querySelector('button[data-action="increment"]'),
+  counterValue: 0,
+};
+const onDecrementBtnClick = () => {
+refs.counterValueRef.textContent = refs.counterValue -= 1;
+};
+
+const onIncrementBtnClick = () => {
+refs.counterValueRef.textContent = refs.counterValue += 1;
+};
+
+refs.decrementBtn.addEventListener("click", onDecrementBtnClick);
+refs.incrementBtn.addEventListener("click", onIncrementBtnClick);
