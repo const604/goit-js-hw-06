be1dbf00f8799ddbda81923c74ca804523a7541f
const604 <116814907+const604@users.noreply.github.com>
2023-02-07 17:58:20 +0200

    6

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,12 @@
 const inputRef = document.querySelector("#validation-input");
-console.log(inputRef);
 
 inputRef.addEventListener("blur", (event) => {
+  const currentActiveValid = document.querySelector(".valid");
+  const currentActiveInvalid = document.querySelector(".invalid");
+  if (currentActiveValid || currentActiveInvalid) {
+    inputRef.classList.remove("valid") || inputRef.classList.remove("invalid");
+  }
   event.currentTarget.value.length == inputRef.dataset.length
-    ? inputRef.classList.add("valid").replace("invalid", "valid")
-    : inputRef.classList.add("invalid").replace("valid", "invalid");
+    ? inputRef.classList.add("valid")
+    : inputRef.classList.add("invalid");
 });
